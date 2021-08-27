16bad49c2fecf7794456306f562f71e213a8ec50
dvyalochkin <dvyalochkin@dtln.ru>
2021-08-27 14:59:28 +0300

    описание инструкции select

---
+++ construction_select.sql
@@ -0,0 +1,9 @@
+[WITH <common_table_expression>]
+SELECT <select_list>
+[INTO <new_table>]
+[FROM <source_table>]
+[WHERE <search_condition>]
+[GROUP BY <group_by_expression>]
+[HAVING <search_condition>]
+[ORDER BY <order_expression> [ASC | DESC]]
+--квадратными скобками выделены не обязательные элементы
