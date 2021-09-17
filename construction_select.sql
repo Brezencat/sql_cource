9f7d1d517e323cdd4538b91779978d0ece42c9af
dvyalochkin <dvyalochkin@dtln.ru>
2021-09-17 11:25:28 +0300

    примеры для select

--- construction_select.sql
+++ construction_select.sql
@@ -1,9 +1,27 @@
-[WITH <common_table_expression>]
-SELECT <select_list>
-[INTO <new_table>]
-[FROM <source_table>]
-[WHERE <search_condition>]
-[GROUP BY <group_by_expression>]
-[HAVING <search_condition>]
-[ORDER BY <order_expression> [ASC | DESC]]
---квадратными скобками выделены не обязательные элементы
+-- [WITH <common_table_expression>]
+-- SELECT <select_list>
+-- [INTO <new_table>]
+-- [FROM <source_table>]
+-- [WHERE <search_condition>]
+-- [GROUP BY <group_by_expression>]
+-- [HAVING <search_condition>]
+-- [ORDER BY <order_expression> [ASC | DESC]]
+-- --квадратными скобками выделены не обязательные элементы
+
+
+--примеры
+
+SELECT NULL
+;
+
+SELECT 1
+;
+
+SELECT 'a'
+;
+
+SELECT getdate()
+;
+
+SELECT 1, 'A', getdate()
+;
